a5282a512aaa83c29800300e7432c12cdc5c55d6
jdrdavinci <jderuijter@davinci.nl>
2021-12-01 14:56:01 +0100

    day1 changes

--- 2021/day1/day1-2.py
+++ 2021/day1/day1-2.py
@@ -2,38 +2,20 @@ file = "/Users/jurnderuijter/projects/AdventOfCode/2021/day1/input-day1.txt"
 #file = "/Users/jurnderuijter/projects/AdventOfCode/2021/day1/input-day1-test.txt"
 
 f = open(file, "r")
+data = f.readlines()
 
-depthOne = 0
-depthTwo = 0 
-depthThree = 0
-
-depthSum = 0
-previousDepthSum = 0
-
-lineCount = 0
+sum = 0
+prevSum = 0
 counter = 0
 
-for x in f:
-    newDepth = int(x.strip())
-    lineCount = lineCount+1
-    mod = lineCount % 3
+for idx, val in enumerate(data):
+    if idx > 1:
+        prevSum = sum
+        sum = int(data[idx].strip()) + int(data[idx-1].strip()) + int(data[idx-2].strip())
+        print(sum)
+        
+        if sum > prevSum and prevSum != 0:
+            counter = counter + 1
 
-    if mod == 1:
-        depthOne = newDepth
-    elif mod == 2:
-        depthTwo = newDepth
-    elif mod == 0:
-        depthThree = newDepth
-
-    if lineCount > 2:
-        previousDepthSum = depthSum
-        depthSum = depthOne + depthTwo + depthThree
-
-        if depthSum > previousDepthSum and previousDepthSum != 0:
-            counter = counter+1
-
-    #print(str(lineCount))
-
-print(str(counter))
-
+print('counter: ' + str(counter))
 print('Done.')
